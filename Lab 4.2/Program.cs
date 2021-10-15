7f0a0fb12473ab8630e5c7599dd8cf68f3e1a3a9
IvanZamurii <zamuriy.it@gmail.com>
2021-10-15 16:52:57 +0300

    Lab 4.2 and 4.3

---
+++ Lab 4.2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Б1 example1 = new Б1();
+            Б2 example2 = new Б2();
+            Б3 example3 = new Б3();
+            string information;
+            information = Б1.argument1;
+            A A1 = new A(information);
+            A.Display();
+            information = Б2.argument2;
+            A A2 = new A(information);
+            A.Display();
+            information = Б3.argument3;
+            A A3 = new A(information);
+            A.Display();
+        }
+    }
+
+    public class Б
+    {
+        protected static string argument;
+        public static void Show()
+        {
+            Console.WriteLine("Введите аргумент: ");
+            argument = Console.ReadLine();
+        }
+    }
+
+    public class Б1 : Б
+    {
+        public static string argument1;
+        public Б1()
+        {
+            Show();
+            argument1 = argument;
+        }
+    }
+
+    public class Б2 : Б
+    {
+        public static string argument2;
+        public Б2()
+        {
+            Show();
+            argument2 = argument;
+        }
+    }
+
+    public class Б3 : Б
+    {
+        public static string argument3;
+        public Б3()
+        {
+            Show();
+            argument3 = argument;
+        }
+    }
+
+    class A
+    {
+        public A(string information)
+        {
+            Input = information;
+        }
+        public static int number, count = 0;
+        public static char symbol;
+        public static bool TrueOrFalse, IsTrue;
+        public static double PointNumber;
+        public static string str;
+        private static string input;
+        public static string Input
+        {
+            get
+            {
+                return input;
+            }
+            set
+            {
+                str = value;
+                IsTrue = int.TryParse(str, out number);
+                if (IsTrue == true)
+                {
+                    value = value + " " + "Тип: число";
+                }
+                IsTrue = double.TryParse(str, out PointNumber);
+                if (IsTrue == true)
+                {
+                    value = value + " " + "Тип: десяткове число";
+                }
+                IsTrue = char.TryParse(str, out symbol);
+                if (IsTrue == true)
+                {
+                    value = value + " " + "Тип: символ";
+                }
+                IsTrue = bool.TryParse(str, out TrueOrFalse);
+                if (IsTrue == true)
+                {
+                    value = value + " " + "Тип: булеан";
+                }
+                if (count == 0)
+                {
+                    value = value + " " + "Тип: стрічка";
+                }
+                input = "Значення: " + value + "Модифікатор доступу: public";
+            }
+        }
+        public static void Display()
+        {
+            Trace.WriteLine(input);
+        }
+    }
+
+
+
+}
---
+++ Lab 4.3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab_4._3
+{
+    class Person
+    {
+        private static string name;
+
+        public static string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public Person()
+        {
+            name = "Ivan";
+        }
+    }
+
+    class A : Person
+    {
+        private static string surname;
+
+        public static string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+        public A()
+        {
+            surname = "Zamurii";
+        }
+    }
+    class Б : A
+    {
+        private static int age;
+        public static int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public Б()
+        {
+            age = 19;
+        }
+
+        static public void Show()
+        {
+            Trace.WriteLine(Name);
+            Trace.WriteLine(Surname);
+            Trace.WriteLine(Age);
+        }
+    }
+    class Program
+    {
+        static public void Main()
+        {
+            Б b = new Б();
+            Б.Show();
+        }
+    }
+}
