fc18d2a557d1669e9c5aa4dd81f830103281ccc7
IvanZamurii <zamuriy.it@gmail.com>
2021-10-15 16:03:05 +0300

    Lab 4.1 and 4.4

---
+++ Lab 4.1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.UTF8;
+            Outputer.Input();
+            Display.Show();
+        }
+    }
+    class Outputer
+    {
+        public static string input;
+        public static void Input()
+        {
+            Console.WriteLine("Введіть дані: ");
+            input = Console.ReadLine();
+        }
+    }
+    class Display : Outputer
+    {
+        private static bool IsTrue;
+        private static bool i;
+        private static int number;
+        private static char symbol;
+        private static double NumberPoint;
+        public static void Check()
+        {
+            IsTrue = int.TryParse(input, out number);
+            if (IsTrue == true)
+            {
+                Trace.WriteLine("Тип: число");
+            }
+            IsTrue = char.TryParse(input, out symbol);
+            if (IsTrue == true)
+            {
+                Trace.WriteLine("Тип: символ");
+            }
+            IsTrue = bool.TryParse(input, out i);
+            if (IsTrue == true)
+            {
+                Trace.WriteLine("Тип: bool");
+            }
+            IsTrue = double.TryParse(input, out NumberPoint);
+            if (IsTrue == true)
+            {
+                Trace.WriteLine("Тип: десяткове число");
+            }
+            Trace.WriteLine("Тип: рядок");
+        }
+        public static void Show()
+        {
+            Trace.WriteLine(input);
+            Check();
+        }
+    }
+}
---
+++ Lab 4.4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab_4._4
+{
+    class ApplicationLicense
+    {
+        private static void AllowTrial()
+        {
+            Console.WriteLine("Trial режим");
+        }
+        private static void AllowCommon()
+        {
+            Console.WriteLine("Безкоштовна версія");
+        }
+        private static void AllowPro()
+        {
+            Console.WriteLine("Платна версія");
+        }
+        public ApplicationLicense(string Key)
+        {
+            if (Key == "431")
+            {
+                AllowTrial();
+            }
+            else if (Key == "785")
+            {
+                AllowPro();
+            }
+            else
+            {
+                AllowCommon();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть ключ доступу (431 - Trial версія, 785 - Повна версія)");
+            Console.WriteLine("Якщо ключ не введений, або введений невірно, користувач отримає доступ до безкоштовної версії");
+            string Key = Console.ReadLine();
+            ApplicationLicense license = new ApplicationLicense(Key);
+        }
+    }
+}
